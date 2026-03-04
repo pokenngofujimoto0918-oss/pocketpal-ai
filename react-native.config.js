238c4f4dd5323d7949c1c4fab07d8e0a863c060c
pokenngofujimoto0918-oss <pokenngofujimoto0918@gmail.com>
2026-03-05 00:29:22 +0900

    AppCheckも除外

--- react-native.config.js
+++ react-native.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '@react-native-firebase/app': { platforms: { ios: null } },
     '@react-native-firebase/crashlytics': { platforms: { ios: null } },
     '@react-native-firebase/analytics': { platforms: { ios: null } },
+    '@react-native-firebase/app-check': { platforms: { ios: null } },
   },
 };
