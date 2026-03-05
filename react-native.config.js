be0045e5674f4bfca80c259c4e1c9aba5dfacfcb
pokenngofujimoto0918-oss <pokenngofujimoto0918@gmail.com>
2026-03-05 23:02:36 +0900

    DeviceInfoとGoogleSignInを除外

--- react-native.config.js
+++ react-native.config.js
@@ -5,5 +5,7 @@ module.exports = {
     '@react-native-firebase/crashlytics': { platforms: { ios: null } },
     '@react-native-firebase/analytics': { platforms: { ios: null } },
     '@react-native-firebase/app-check': { platforms: { ios: null } },
+    'react-native-device-info': { platforms: { ios: null } },
+    '@react-native-google-signin/google-signin': { platforms: { ios: null } },
   },
 };
